40d69d08678e18a5153c85d1e22474b1ae2a57d5
brunocasanova <deftonicks_me@hotmail.com>
2015-04-21 11:11:18 +0100

    AUTO-COMMIT 21/04/15 at 11:11:18

--- client/router.js
+++ client/router.js
@@ -7,7 +7,15 @@ Router.route( '/', { name: 'firstPage' });
 
 // Pages
 Router.route( '/right', { name: 'rightPage' });
+
+// Up
 Router.route( '/over_up', { name: 'overUp' });
+
+// Down
 Router.route( '/over_down', { name: 'overDown' });
+
+// Left
 Router.route( '/left', { name: 'leftPage' });
+
+// Fade
 Router.route( '/cross_fade', { name: 'crossFade' });
